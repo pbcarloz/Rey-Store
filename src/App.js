e54c8f9bd109ac0f96cedfabe18cc0b906dbba4d
pbcarloz <pb_carlos@outlook.com>
2022-04-12 21:25:33 -0500

    updated categories

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
-
+import './components/categories/categories-menu.jsx'
+import CategoriesMenu from './components/categories/categories-menu.jsx';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Shoes',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    },
-  ]
   return (
-
-    <div className="categories-container">
-      {categories.map(({title}) => (      
-        <div className="category-container">
-          {/* <img /> */}
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      
-
-      ))}
-    </div>
+  <CategoriesMenu />
   );
 }
 
