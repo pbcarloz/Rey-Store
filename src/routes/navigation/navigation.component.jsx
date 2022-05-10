58e7b353ee3b91d6836b92f30e4d331347af8ffa
pbcarloz <pb_carlos@outlook.com>
2022-05-09 21:50:18 -0500

    Added context for cart, shopping list and cart icont

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom"
 import { ReactComponent as ReyLogo } from '../../assets/crown.svg';
 import { UserContext } from "../../context/user.context";
+import { CartContext } from "../../context/cart.context";
 import { signOutUser } from '../../utils/firebase/firebase.utils.js';
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
@@ -11,7 +12,7 @@ import './navigation.styles.scss'
 const Navigation = () => {
 
     const { currentUser } = useContext(UserContext);
-    
+    const {isCartOpen} = useContext(CartContext)
 
     return (
       <Fragment>
@@ -38,7 +39,7 @@ const Navigation = () => {
                 <CartIcon />
                 
             </div>
-                <CartDropdown />
+                {isCartOpen && <CartDropdown />}
         </div>
           <Outlet />
       </Fragment>
